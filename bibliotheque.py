7ab4334d3101d5d2a3376c7e38f43947820c948d
RamaAbdoulaye <dialloramatoulayebah@gmail.com>
2025-05-17 10:15:16 -0400

    Ajout du message de l'affichage du menu

---
+++ bibliotheque.py
@@ -0,0 +1,16 @@
+
+
+def afficherMenu():
+    print("\t  - Gestion biblliotheque Ramatoulaye Diallo -")
+    print("\t | 1. Afficher tous les livres                |")
+    print("\t | 2. Ajouter un livre                        |")
+    print("\t | 3. Supprimer un livre                      |")
+    print("\t | 4. Rechercher un livre comme lu            |")
+    print("\t | 5. Marquer un livre comme lu               |")
+    print("\t | 6. Afficher livres lus non lus             |")
+    print("\t | 7. Trier les livres                        |")
+    print("\t |8. Quitter                                  |")
+    print("\t ---------------------------------------------")
+
+
+afficherMenu()
